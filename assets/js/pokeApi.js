d36f91f2fb8b73fdfdf2d65da833079bdef79021
AndreAugustoDev <andreaugustoqueiroz999@gmail.com>
2024-01-19 23:09:12 -0300

    refactor: improve pokeApi code

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,35 +1,32 @@
 const pokeApi = {};
 
 const convertPokeApiDetailToPokemon = (pokeDetail) => {
+	const { id, name, types, sprites } = pokeDetail;
 	const pokemon = new Pokemon();
-	pokemon.number = pokeDetail.id;
-	pokemon.name = pokeDetail.name;
-
-	const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
-	const [type] = types;
-
-	pokemon.types = types;
-	pokemon.type = type;
-
-	pokemon.sprite = pokeDetail.sprites.other["official-artwork"].front_default;
-
+	pokemon.number = id;
+	pokemon.name = name;
+	pokemon.types = types.map((typeSlot) => typeSlot.type.name);
+	pokemon.type = pokemon.types[0];
+	pokemon.sprite = sprites.other["official-artwork"].front_default;
 	return pokemon;
 };
-pokeApi.getPokemonDetails = (pokemon) => {
-	return fetch(pokemon.url)
-		.then((response) => response.json())
-		.then(convertPokeApiDetailToPokemon);
+
+pokeApi.getPokemonDetails = async (pokemon) => {
+	const response = await fetch(pokemon.url);
+	return convertPokeApiDetailToPokemon(await response.json());
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemon = async (offset = 0, limit = 10) => {
 	const api = "https://pokeapi.co/api/v2/pokemon";
 	const url = `${api}?offset=${offset}&limit=${limit}`;
-
-	return fetch(url)
-		.then((response) => response.json())
-		.then((data) => data.results)
-		.then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
-		.then((detailsRequest) => Promise.all(detailsRequest))
-		.then((pokemonsDetails) => pokemonsDetails)
-		.catch((error) => console.log(error));
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		const pokemon = data.results;
+		const detailsRequest = pokemon.map(pokeApi.getPokemonDetails);
+		const pokemonDetails = await Promise.all(detailsRequest);
+		return pokemonDetails;
+	} catch (error) {
+		console.log(error);
+	}
 };
